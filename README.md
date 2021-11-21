365f8ce45a08bcb4eb8b52d416b17450ea60a5d9
TheMadMike <m.gibas2407@gmail.com>
2021-11-22 00:13:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# 🍴 Culinary app
+
+App for managing products and recipes.
+(University assignment)
+
+## 📒 Dependencies
+
+- Java 15+
+- Maven
+
+## ⚗️ Build and run
+
+```shell
+mvn clean javafx:run
+```
---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application {
+
+    private static final String WINDOW_TITLE = "Culinary App";
+    private static final String FXML_FILE_NAME = "App.fxml";
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = loadFXML();
+        stage.setScene(new Scene(root));
+        stage.setTitle(WINDOW_TITLE);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private Parent loadFXML() {
+        try {
+            URL resourceUrl = getClass().getResource(FXML_FILE_NAME);
+            return FXMLLoader.load(resourceUrl);
+        } catch(IOException exception) {
+            System.err.println(exception.getMessage());
+        }
+        return new Group();
+    }
+
+}
