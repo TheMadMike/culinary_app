5e6c4600dc6988852d19a5bf474c13a6164d1fba
TheMadMike <m.gibas2407@gmail.com>
2021-11-25 14:43:53 +0100

    Remove unused imports

--- src/main/java/controller/ProductsController.java
+++ src/main/java/controller/ProductsController.java
@@ -5,7 +5,6 @@ import data.DataRepository;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -14,7 +13,6 @@ import javafx.scene.layout.VBox;
 import model.Product;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
